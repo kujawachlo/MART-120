f2b6f4d437c55db85939d56c451d4e40a40bce0b
chloe kujawa <chloechk0@gmail.com>
2024-10-28 13:15:34 -0600

    Add files via upload

---
+++ HOMEWORK 9/sketch.js
@@ -0,0 +1,35 @@
+function setup() 
+{
+  createCanvas(300, 400);
+}
+
+function draw() 
+{
+background(225,204,0);
+
+fill("#fae")
+  circle(150,180,250);
+
+  square(150,150,55,10);
+square(50,150,50,10);
+    fill(255,255,255)
+    ellipse(80,180,35,15);
+    ellipse(175,180,45,15);
+    rect(105,270,50,5);
+    fill("red")
+    triangle(100, 235, 150, 220, 125, 175);
+    point(80, 180);
+    point(170, 180);
+    line(20, 190, 85, 75);
+    line(250, 190, 195, 65);
+    line(150, 180, 100, 180);
+    line(300, 350, 255, 250);
+      line(255, 350, 235, 270);
+    line(20, 350, 45, 250);
+      line(50, 350, 65, 270);
+    line(100, 130, 50, 130);
+    line(150, 130, 200, 130);
+
+fill("black")
+  text('Chloe Kujawa', 110, 30);
+}
